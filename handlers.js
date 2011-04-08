f54a6e0a2704de809104277878c379fc08e3c318
Achiel  van der Mandele <achiel@ismoke.local>
2011-04-08 23:30:19 +0200

    first couchdb link!  * renamed express var -> server

--- handlers.js
+++ handlers.js
@@ -1,9 +1,10 @@
 var cradle = require('cradle');
 var json = require('./json2');
 var superkoud = require('./superkoud');
+var http = require('http');
 var express = require('express');
-var http = express.createServer();
-http.use(express.bodyParser());
+var server = express.createServer();
+server.use(express.bodyParser());
 
 var users = ['achiel', 'muriel'];
 var username = 'achiel';
@@ -17,13 +18,13 @@ function checkSession(req, res, next)
         next(new Error('User not logged in'));
 }
 
-http.get('/', 
+server.get('/', 
     function(req, res)
     {
         res.send('hello world');
     }
 );
-http.get('/user/', 
+server.get('/user/', 
     function(req, res)
     {
         console.log('get all users');
@@ -31,38 +32,67 @@ http.get('/user/',
     }
 );
 
-http.post('/user/', //checkSession,
-        function (req, res)
-        {
-          console.log('post users');
-          for (var key in req.body) {
-              if (req.body.hasOwnProperty(key)) {
-                  console.log(key);
-              }
-          }
-          raw_json = req.body.data;
-          console.log("found raw json " + raw_json);
-          decoded = JSON.parse(raw_json);
-
-          console.log(decoded);
-          superkoud.createUser(decoded.username);
-          res.send('flup');
-        }
-);
-http.get('/user/:id', 
-    function(req, res)
+server.post('/user/', //checkSession,
+    function (req, res)
     {
-        user = superkoud.getUser(req.params.id);
-        res.send(user);
+        console.log('post users');
+        for (var key in req.body) 
+        {
+            if (req.body.hasOwnProperty(key)) 
+            {
+                console.log(key);
+            }
+        }
+        raw_json = req.body.data;
+        console.log("found raw json " + raw_json);
+        decoded = JSON.parse(raw_json);
+
+        console.log(decoded);
+        superkoud.createUser(decoded.username);
+        res.send('flup');
     }
 );
-http.get('/user/:id/:key', 
+server.get('/user/:id', 
+    function(req, res)
+    {
+        var c = new(cradle.Connection)('g', 5984, {
+    cache: true,
+    raw: false
+});
+        console.log(c);
+        var db = c.database('superkoud');
+
+        db.get('achiel', function (err, doc) {
+            console.log(doc);
+            res.send(doc);
+        });
+    }
+);
+server.get('/user/:id/:key', 
     function(req, res)
     {
         user = superkoud.getUser(req.params.id);
         res.send(user[req.params.key]);
     }
 );
+server.post('/user/:id/:key/', checkSession,
+    function(req, res)
+    {
+        user = superkoud.getUser(req.params.id);
+        raw_data = req.body['data'];
+        data = JSON.parse(raw_data);
+        user[req.params.key].push(data);
+        console.log(user[req.params.key]);
+        res.send('ok!');
+    }
+);
+server.delete('/user/:id/:key', checkSession,
+    function(req, res)
+    {
+        user = superkoud.getUser(req.params.id);
+        delete user[req.params.key];
+        res.send('ok!');
+    }
+);
 
-
-exports.server = http;
+exports.server = server;
