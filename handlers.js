7ff203c8d2a0a2de22f017c032fbce270252c249
Achiel  van der Mandele <achiel@ismoke.local>
2011-04-22 22:39:27 +0200

    removed all auth crap, added static module

--- handlers.js
+++ handlers.js
@@ -1,32 +1,23 @@
-var superkoud = require('./superkoud');
-var express = require('express');
-
-var server = express.createServer();
-server.use(express.bodyParser());
+var superkoud = require('./superkoud'),
+express = require('express'),
+connect = require('connect'),
+static = require('node-static');
 
 var username = 'achiel';
 
-function checkSession(req, res, next)
-{
-    if (username == 'achiel')
-        next();
-    else
-        next(new Error('User not logged in'));
-}
-
-server.get('/', 
-    function(req, res)
-    {
-        res.send('hello world');
-    }
+var file = new(static.Server)('./forms/');
+var server = express.createServer(
 );
+server.use(express.static(__dirname + '/static'));
+server.use(express.bodyParser());
+
 server.get('/user/', superkoud.getAllUsers);
 
 server.get('/user/:id', superkoud.getUser);
 server.get('/user/:id/:key', superkoud.getAttribute);
-server.put('/user/:id/:key/', checkSession, superkoud.editAttribute);
-server.post('/user/:id/:key/', checkSession, superkoud.createAttribute);
-server.delete('/user/:id/:type/:key', checkSession, superkoud.deleteAttribute);
+server.put('/user/:id/:key/', superkoud.editAttribute);
+server.post('/user/:id/:key/', superkoud.createAttribute);
+server.delete('/user/:id/:type/:key', superkoud.deleteAttribute);
 
 
 exports.server = server;
