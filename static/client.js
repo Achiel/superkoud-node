aa7af8086e96ad5f86f1509a0b3a206ef9b59fd6
Achiel  van der Mandele <achiel@ismoke.local>
2011-04-23 23:42:36 +0200

    implemented view all users

--- static/client.js
+++ static/client.js
@@ -9,6 +9,21 @@ define(function(require, exports, module) {
                     $("#form").html(html);
                 }); 
         } 
+        exports.viewUsers = function() {
+            $.get("/user/", function(data) {
+                console.log(data);
+                $.get("/tips.html", function(tipform) {
+                    for (user in data) {
+                        console.log("going after " + data[user]);
+                        $.get("/user/" + data[user], function(user) {
+                            var html = Mustache.to_html(tipform, user);
+                            $("#tips").append(html);
+                            console.log(user);
+                        });
+                    }
+                });
+            });
+        }
         console.log("superkoud-js loaded..");
     }
 );
